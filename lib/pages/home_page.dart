01e0b030429e68597eaa9a3a564d950cebecaefa
pipipi <202100355d@utea.edu.pe>
2024-10-16 13:25:53 -0500

    home

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+const HomePage({Key? key}): super(key: key);
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: Text("Firebase Firestore"),
+),
+// AppBar
+); // Scaffold
+}
+}
